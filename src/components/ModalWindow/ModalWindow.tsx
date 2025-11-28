bba2e54dc89b967fd1e7adb4bd4f4f4542571eee
Vladislav Shitov <sayroxskrim@bk.ru>
2025-11-28 14:52:26 +0400

    fix add user

--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -10,7 +10,8 @@ import {addVisitor, editVisitor, removeVisitor} from "../../store/slices/visitor
 const ModalWindow = () => {
 
     const dispatch = useDispatch<AppDispatch>();
-    const idCount = useSelector((state: RootState) => state.visitors.visitors.length);
+    const visitors = useSelector((state: RootState) => state.visitors.visitors);
+    const idCount = visitors.length > 0 ? visitors[visitors.length-1].id : 0
     const isEditOpen = useSelector((state: RootState) => state.isModelOpen.edit);
     const whatVisitorOpenToEdit = useSelector((state: RootState) => state.isModelOpen.currentVisitor);
     const [selectorOpen, setSelectorOpen] = useState<boolean>(false)
